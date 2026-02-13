b59a5909cc53d65a6b4f0e89cbf6e3403ab9fe45
Mykola Sheiko <nyr3mvneov@gmail.com>
2026-02-13 07:53:43 +0000

    refactor: move students data to a separate module

--- index.js
+++ index.js
@@ -1,46 +1,9 @@
 const path = require("path");
 const express = require("express");
+const students = require("./students");
 
 const app = express();
 const PORT = 3000;
-
-const students = [
-  {
-    id: 1,
-    name: "Шейко Микола",
-    role: "Team Lead",
-    group: "ІА-44",
-    hobby: "Веб-розробка"
-  },
-  {
-    id: 2,
-    name: "Несміян Олександр",
-    role: "Frontend Developer",
-    group: "ІА-44",
-    hobby: "Диван"
-  },
-  {
-    id: 3,
-    name: "Гладіголов Дмитро",
-    role: "Backend Developer",
-    group: "ІА-44",
-    hobby: "---------"
-  },
-  {
-    id: 4,
-    name: "Буцик Богдан",
-    role: "QA Engineer",
-    group: "ІА-44",
-    hobby: "--------"
-  },
-  {
-    id: 5,
-    name: "Лобко Олексій",
-    role: "DevOps",
-    group: "ІА-44",
-    hobby: "---------"
-  }
-];
 
 app.set("view engine", "ejs");
 
---
+++ students.js
@@ -0,0 +1,39 @@
+const students = [
+  {
+    id: 1,
+    name: "Шейко Микола",
+    role: "Team Lead",
+    group: "ІА-44",
+    hobby: "Веб-розробка"
+  },
+  {
+    id: 2,
+    name: "Несміян Олександр",
+    role: "Frontend Developer",
+    group: "ІА-44",
+    hobby: "Диван"
+  },
+  {
+    id: 3,
+    name: "Гладіголов Дмитро",
+    role: "Backend Developer",
+    group: "ІА-44",
+    hobby: "---------"
+  },
+  {
+    id: 4,
+    name: "Буцик Богдан",
+    role: "QA Engineer",
+    group: "ІА-44",
+    hobby: "--------"
+  },
+  {
+    id: 5,
+    name: "Лобко Олексій",
+    role: "DevOps",
+    group: "ІА-44",
+    hobby: "---------"
+  }
+];
+
+module.exports = students;
